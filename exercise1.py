2cfd7987b99d39e463977789808538d1dbcaf122
Asli Shemesh <aslishemesh@gmail.com>
2017-03-21 18:21:58 +0200

    Adding soulution for exe1 - training

---
+++ exercise1.py
@@ -0,0 +1,26 @@
+# Exercise1 - training.
+
+
+def check_div_even(num, check):
+    if num % check == 0:
+        return True
+    else:
+        return False
+
+
+num = input("Please enter a number: ")
+
+if check_div_even(num,4) == True:
+    print "The number %d can be divided by 4" % num
+elif check_div_even(num, 2) == True:
+    print "The number %d is an even number but not a multiple of 4" % num
+else:
+    print "The number %d is an odd number and therefor cannot be divided by 2 or 4" % num
+
+check = input("please enter a number to check (as a divider): ")
+
+if check_div_even(num,check) == True:
+    print "The number %d can be divided by %d" % (num, check)
+else:
+    print "The number %d cannot be divided by %d" % num % check
+
