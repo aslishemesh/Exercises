36bfe79ee769f5b0a2edb0033aafb36727a438d4
Asli Shemesh <aslishemesh@gmail.com>
2017-03-27 13:36:10 +0300

    Adding solution for exe5 (update) - training

--- exercise5.py
+++ exercise5.py
@@ -9,17 +9,31 @@ Fn+1 = Fn+Fn-1
 #       to change the program to support F1=F2=1 we need to
 #       change the condition to if(num==1 or num==2) - return 1
 
+from array import *
+
 def fib(num):
     if num == 0:
         return 0
     elif num == 1:
         return 1
-    else:
-        a = fib(num-1) + fib(num-2)
-        return a
+
+    return fib(num-1) + fib(num-2)
+
 
 num = input("Please enter a number: ")
 
+a = array('i',[0,1])
+for i in range(num-2):
+    a.append(a[i+1] + a[i])
+print "[array] The Fibonacci sequence for f(",num,") is:"
+print a.tolist()
+
+
+
+num = input("Please enter a number: ")
+print "[recursive] The Fibonacci number for f(",num,") is", fib(num)
+
+print "[recursive] The Fibonacci sequence for f(",num,") is:"
 for i in range(num):
     print fib(i)
 
